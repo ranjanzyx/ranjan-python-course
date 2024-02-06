b42d0a4560d7dac8685afac75b1babef6995e651
ranjanzyx <ranjanzyx@gmail.com>
2024-02-06 18:29:51 +0530

    Python Envs

---
+++ day20_21_python_env_pip_libs_venvs/main.py
@@ -0,0 +1,6 @@
+from utilities import calculate_average
+
+# Example usage of calculate_average
+numbers = [23, 45, 67, 89]
+average = calculate_average(numbers)
+print(f"The average of the numbers is: {average}")
